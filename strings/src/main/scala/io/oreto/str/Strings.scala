0933505cce659000c0f2b10f3b3d90e6510d0283
Ross Oreto <ross.oreto@gmail.com>
2020-04-01 16:17:40 -0500

    Separate CharSequence and String implicit classes.

--- strings/src/main/scala/io/oreto/str/Strings.scala
+++ strings/src/main/scala/io/oreto/str/Strings.scala
@@ -5,6 +5,7 @@ import io.oreto.num.Numbers
 object Strings {
 
   final val EMPTY = ""
+  final val SPACE = " "
 
   object Chars {
     val NEGATIVE = '-'
@@ -12,32 +13,36 @@ object Strings {
     val DECIMAL = '.'
   }
 
-  implicit class Str(val s: String) {
-
+  implicit class CharSeq(val s: CharSequence) {
     def isNumber(t: Numbers.Type.Value = Numbers.Type.rational): Boolean = {
       // do some initial sanity checking to catch easy issues quickly and with very little processing
       // also this makes the state tracking in the case statements
+      val length = s.length
+      if(s.isBlank
+        || (length == 1 && !s.charAt(0).isDigit)
+        || s.charAt(length - 1) == Chars.DECIMAL)
+        return false
+
       var dotted = false
-      val valid = !(s.isBlank
-        || (s.length == 1 && !s.charAt(0).isDigit)
-        || s.endsWith(String.valueOf(Chars.DECIMAL))) &&
-        s.zipWithIndex.forall(c => {
-        c._1 match {
+      val valid =
+        0.until(length).forall(i => {
+          val c = s.charAt(i)
+        c match {
           // negative can only be in the start position
-          case Chars.NEGATIVE if c._2 == 0 && (t == Numbers.Type.rational || t == Numbers.Type.integer) => true
+          case Chars.NEGATIVE if i == 0 && (t == Numbers.Type.rational || t == Numbers.Type.integer) => true
           case Chars.NEGATIVE => false
-          case Chars.POSITIVE if c._2 == 0 => true
+          case Chars.POSITIVE if i == 0 => true
           case Chars.POSITIVE => false
           // only one decimal place allowed
           case Chars.DECIMAL if dotted || t != Numbers.Type.rational => false
           case Chars.DECIMAL => dotted = true; true
           // this better be a digit
-          case _ if c._1.isDigit => true
+          case _ if c.isDigit => true
           case _ => false
         }
       })
       // make sure natural number type isn't assigned a 0
-      if (valid && t == Numbers.Type.natural && s.replaceAll("^0+", EMPTY).length == 0) false
+      if (valid && t == Numbers.Type.natural && s.chars.allMatch(_ == '0')) false
       else valid
     }
 
@@ -49,8 +54,14 @@ object Strings {
       s == "true" || s == "false"
     }
 
+    def isBlank: Boolean = {
+      s.length == 0 || s.chars().allMatch(Character.isWhitespace(_))
+    }
+  }
+
+  implicit class Str(val s: String) {
     def isByte: Boolean = {
-      if (isInteger) {
+      if (s.isInteger) {
         val i = s.toInt
         i >= Byte.MinValue && i <= Byte.MaxValue
       } else false
--- strings/src/test/scala/io/oreto/StringsTest.scala
+++ strings/src/test/scala/io/oreto/StringsTest.scala
@@ -1,35 +1,35 @@
 package io.oreto
 
 import io.oreto.num.Numbers
-import org.scalatest._
 import io.oreto.str.Strings._
+import org.scalatest._
 import org.scalatest.matchers.should.Matchers
 
 class StringsTest extends FlatSpec with Matchers {
 
   "A number string" should "be a rational number" in {
     "1".isNumber() should be (true)
-    "+10". isNumber() should be (true)
-    "33.00". isNumber() should be (true)
-    "-2.0". isNumber() should be (true)
-    ".5". isNumber() should be (true)
-    "-.3140". isNumber() should be (true)
-    "-19.222222". isNumber() should be (true)
+    "+10".isNumber() should be (true)
+    "33.00".isNumber() should be (true)
+    "-2.0".isNumber() should be (true)
+    ".5".isNumber() should be (true)
+    "-.3140".isNumber() should be (true)
+    "-19.222222".isNumber() should be (true)
 
-    "two". isNumber() should be (false)
-    "thirty-one". isNumber() should be (false)
-    "5.0.0". isNumber() should be (false)
-    "-2.-". isNumber() should be (false)
-    "10.". isNumber() should be (false)
-    "-3-2". isNumber() should be (false)
-    "0-". isNumber() should be (false)
-    "-". isNumber() should be (false)
-    "+". isNumber() should be (false)
-    ".". isNumber() should be (false)
-    "-.". isNumber() should be (false)
-    "+.". isNumber() should be (false)
-    "". isNumber() should be (false)
-    "    ". isNumber() should be (false)
+    "two".isNumber() should be (false)
+    "thirty-one".isNumber() should be (false)
+    "5.0.0".isNumber() should be (false)
+    "-2.-".isNumber() should be (false)
+    "10.".isNumber() should be (false)
+    "-3-2".isNumber() should be (false)
+    "0-".isNumber() should be (false)
+    "-".isNumber() should be (false)
+    "+".isNumber() should be (false)
+    ".".isNumber() should be (false)
+    "-.".isNumber() should be (false)
+    "+.".isNumber() should be (false)
+    "".isNumber() should be (false)
+    "    ".isNumber() should be (false)
   }
   
   "An integer" should "be in the set { -n...0...n }" in {
